c20d6344aad418751710db882c9d28285c1bf59a
adrianepedroso <adrianeped1995@gmail.com>
2021-01-11 17:27:38 -0300

    Aula funcoes

--- aula-javascript/js/main.js
+++ aula-javascript/js/main.js
@@ -1,3 +1,56 @@
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigada por clicar";
+    
+    //alert("Obrigada por clicar!");
+}
+
+function redirecionar(){
+    window.open("https://github.com/adrianeped");
+    //window.location.href = "https://github.com/adrianeped"; //abre na mesma janela
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigada por passar o mouse";
+    elemento.innerHTML = "Obrigada por passar o mouse"
+    //alert("trocar texto");
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui"
+}
+
+function load(){
+    alert("Página carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+
+//////AULA 2
+/* //FUNÇÃO
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+function validaIdade(idade){
+    var validar; //se está dentro da funcao ele só existe aqui (local)
+    
+    if (idade >= 18){
+        validar = true;
+    } else {
+        validar = false;
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade");
+console.log(validaIdade(idade));
+*/
+
+/////AULA 1
 /* //DECLARANDO VARIÁVEL E IMPRIMINDO A INFO
 var nome = "Adriane Antunes Pedroso";
 var idade = 25;
@@ -68,8 +121,3 @@ alert(d.getTime());
 alert(d.getHours());
 alert(d.getDay());
 */
-
-var d = "d";
-var e = "e";
-
-console.log (d + e + 1 + "-" + 22);
